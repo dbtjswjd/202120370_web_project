0836c09884ceb4f5c2016f35a4c3121376f4c7ea
dbtjswjd <163017875+dbtjswjd@users.noreply.github.com>
2024-06-16 03:24:21 +0900

    date cover

---
+++ script/script.js
@@ -0,0 +1,21 @@
+setTimeout(function () {
+  document.getElementById('odometer0').innerHTML = '2';
+}, 1000);
+
+setTimeout(function () {
+  document.getElementById('odometer1').innerHTML = '4';
+}, 1500);
+
+setTimeout(function () {
+  document.getElementById('odometer3').innerHTML = '8';
+}, 2000);
+
+setTimeout(function () {
+  document.getElementById('odometer4').innerHTML = '1';
+}, 1000);
+
+setTimeout(function () {
+  var element = document.getElementById('odometer5');
+  element.innerHTML = '6';
+  element.classList.add('moveLeft');
+}, 1820);
