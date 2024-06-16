627a5a15a98866cf0fb9dd78decdca3787564f42
dbtjswjd <163017875+dbtjswjd@users.noreply.github.com>
2024-06-16 10:50:42 +0900

    covwe_4

--- script/script.js
+++ script/script.js
@@ -17,10 +17,26 @@ setTimeout(function () {
 setTimeout(function () {
   var element = document.getElementById('odometer5');
   element.innerHTML = '6';
-  element.classList.add('moveLeft');
+  // element.classList.add('moveLeft');
 }, 1830);
 
 setTimeout(function () {
   var element = document.querySelector('.screen_start');
   element.classList.add('shrinkAnimation');
 }, 5000);
+
+setTimeout(function () {
+  var element = document.querySelector('.screen_start');
+  element.classList.add('shrinkAnimation');
+
+  // 애니메이션이 끝난 후 h1 요소를 페이드인 처리
+  setTimeout(function () {
+    var h1Element = document.querySelector('h1');
+    h1Element.classList.add('fade-in');
+  }, 500); // 500ms 후에 실행 (애니메이션 duration에 맞게 조정)
+
+  setTimeout(function () {
+    var h1Element = document.querySelector('h3');
+    h1Element.classList.add('fade-in');
+  }, 500);
+}, 4000);
