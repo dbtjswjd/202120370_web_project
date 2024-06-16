4257ceb53a793b076127d083b43d2de3a7d69783
dbtjswjd <163017875+dbtjswjd@users.noreply.github.com>
2024-06-16 16:06:19 +0900

    cover_almost

--- script/script.js
+++ script/script.js
@@ -40,3 +40,28 @@ setTimeout(function () {
     h1Element.classList.add('fade-in');
   }, 500);
 }, 4000);
+
+$(window).on('load', function () {
+  // h1 요소를 페이드 인 처리
+  setTimeout(function () {
+    var h1Element = document.querySelector('h1.start_title');
+    h1Element.classList.add('fade-in');
+  }, 500); // 500ms 후에 실행 (로딩 애니메이션에 맞춰 조정)
+
+  // h3 요소를 페이드 인 처리
+  setTimeout(function () {
+    var h3Element = document.querySelector('h3.start_subtitle');
+    h3Element.classList.add('fade-in');
+  }, 500); // 500ms 후에 실행 (로딩 애니메이션에 맞춰 조정)
+
+  // 로딩 화면을 페이드 아웃 후 완전히 제거
+  setTimeout(function () {
+    $('#loadingScreen').fadeOut(1000, function () {
+      $('#loadingScreen').css('pointer-events', 'none'); // 이벤트 차단
+      $('#loadingScreen').remove(); // 페이드 아웃 후 요소를 DOM에서 제거
+
+      // 메인 콘텐츠를 부드럽게 표시 (페이드 인 애니메이션)
+      $('#mainContent').fadeIn(1000); // 1초 동안 페이드 인 애니메이션 적용
+    });
+  }, 4000); // 총 4.5초 후에 실행 (초기 애니메이션 시간 포함)
+});
