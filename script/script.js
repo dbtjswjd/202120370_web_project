2b1da3c190a658c3b9fe6179e4aa714cf6486037
dbtjswjd <163017875+dbtjswjd@users.noreply.github.com>
2024-06-16 17:16:19 +0900

    web-cover-done

--- script/script.js
+++ script/script.js
@@ -41,27 +41,37 @@ setTimeout(function () {
   }, 500);
 }, 4000);
 
-$(window).on('load', function () {
-  // h1 요소를 페이드 인 처리
+setTimeout(function () {
+  var loadingScreen = document.getElementById('loadingScreen');
+  loadingScreen.classList.add('fade-out');
   setTimeout(function () {
-    var h1Element = document.querySelector('h1.start_title');
-    h1Element.classList.add('fade-in');
-  }, 500); // 500ms 후에 실행 (로딩 애니메이션에 맞춰 조정)
+    var mainContent = document.getElementById('mainContent');
+    mainContent.style.display = 'block'; // 요소를 보이도록 설정
+    setTimeout(function () {
+      mainContent.classList.add('visible');
+    }, 50); // 50밀리초 후에 visible 클래스 추가
+  }, 1000); // 1초 후에 요소를 보이게 하고 fade-in 애니메이션 시작
+}, 8000);
 
-  // h3 요소를 페이드 인 처리
-  setTimeout(function () {
-    var h3Element = document.querySelector('h3.start_subtitle');
-    h3Element.classList.add('fade-in');
-  }, 500); // 500ms 후에 실행 (로딩 애니메이션에 맞춰 조정)
+// $(window).on('load', function () {
+//   // 로딩 화면을 페이드 아웃 후 메인 콘텐츠를 페이드 인
+//   setTimeout(function () {
+//     $('#loadingScreen').fadeOut(1000, function () {
+//       $('#loadingScreen').css('pointer-events', 'none'); // 이벤트 차단
+//       $('#loadingScreen').remove(); // 페이드 아웃 후 요소를 DOM에서 제거
 
-  // 로딩 화면을 페이드 아웃 후 완전히 제거
-  setTimeout(function () {
-    $('#loadingScreen').fadeOut(1000, function () {
-      $('#loadingScreen').css('pointer-events', 'none'); // 이벤트 차단
-      $('#loadingScreen').remove(); // 페이드 아웃 후 요소를 DOM에서 제거
+//       // 메인 콘텐츠를 부드럽게 표시 (페이드 인 애니메이션)
+//       $('#mainContent').fadeIn(1000); // 1초 동안 페이드 인 애니메이션 적용
+//     });
+//   }, 4000); // 4초 후에 실행
+// });
 
-      // 메인 콘텐츠를 부드럽게 표시 (페이드 인 애니메이션)
-      $('#mainContent').fadeIn(1000); // 1초 동안 페이드 인 애니메이션 적용
-    });
-  }, 4000); // 총 4.5초 후에 실행 (초기 애니메이션 시간 포함)
-});
+// setTimeout(function () {
+//   $('#loadingScreen').fadeOut(9000, function () {
+//     $('#loadingScreen').css('pointer-events', 'none'); // 이벤트 차단
+//     $('#loadingScreen').remove(); // 페이드 아웃 후 요소를 DOM에서 제거
+
+//     // 메인 콘텐츠를 부드럽게 표시 (페이드 인 애니메이션)
+//     $('#mainContent').fadeIn(1000); // 1초 동안 페이드 인 애니메이션 적용
+//   });
+// }, 6500); // 총 6.5초 후에 실행 (초기 애니메이션 시간 포함)
--- script1/script1.js
+++ script1/script1.js
@@ -1,19 +1,19 @@
-$(window).on('load', function () {
-  // 로딩 화면을 페이드 아웃 후 메인 콘텐츠를 페이드 인
-  $('#loadingScreen').fadeOut(500, function () {
-    $('#mainContent').fadeIn(1000); // 1초 동안 페이드 인 애니메이션 적용
-    $(this).remove(); // 페이드 아웃 후 요소를 DOM에서 제거
-  });
+// $(window).on('load', function () {
+//   // 로딩 화면을 페이드 아웃 후 메인 콘텐츠를 페이드 인
+//   $('#loadingScreen').fadeOut(500, function () {
+//     $('#mainContent').fadeIn(1000); // 1초 동안 페이드 인 애니메이션 적용
+//     $(this).remove(); // 페이드 아웃 후 요소를 DOM에서 제거
+//   });
 
-  // // h1 요소를 페이드 인 처리
-  // setTimeout(function () {
-  //   var h1Element = document.querySelector('h1.start_title');
-  //   h1Element.classList.add('fade-in');
-  // }, 500); // 500ms 후에 실행 (로딩 애니메이션에 맞춰 조정)
+//   // h1 요소를 페이드 인 처리
+//   setTimeout(function () {
+//     var h1Element = document.querySelector('h1.start_title');
+//     h1Element.classList.add('fade-in');
+//   }, 500); // 500ms 후에 실행 (로딩 애니메이션에 맞춰 조정)
 
-  // // h3 요소를 페이드 인 처리
-  // setTimeout(function () {
-  //   var h3Element = document.querySelector('h3.start_subtitle');
-  //   h3Element.classList.add('fade-in');
-  // }, 500); // 500ms 후에 실행 (로딩 애니메이션에 맞춰 조정)
-});
+//   // h3 요소를 페이드 인 처리
+//   setTimeout(function () {
+//     var h3Element = document.querySelector('h3.start_subtitle');
+//     h3Element.classList.add('fade-in');
+//   }, 500); // 500ms 후에 실행 (로딩 애니메이션에 맞춰 조정)
+// });
