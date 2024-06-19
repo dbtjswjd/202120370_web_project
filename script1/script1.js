6dba84c9d7f46e7427427436634e79472efd859c
dbtjswjd <163017875+dbtjswjd@users.noreply.github.com>
2024-06-19 18:11:10 +0900

    final_3

--- script1/script1.js
+++ script1/script1.js
@@ -39,21 +39,38 @@ function adjustImageSize() {
 }
 //메인 이미지 자동 변경 애니메이션
 
+// window.addEventListener('scroll', handleScroll);
+
+// function handleScroll() {
+//   let text1Typing = document.querySelector('.text1_typing');
+//   let bounding = text1Typing.getBoundingClientRect();
+//   let viewportHeight = window.innerHeight;
+
+//   // 스크롤 시 중간쯤 위치할 때 애니메이션 시작 / 1부분일딴 1로해놓고 중간읋 할떄 2로 변경필요
+//   if (bounding.top < viewportHeight / 3) {
+//     setTimeout(function () {
+//       text1Typing.classList.add('show');
+//       setTimeout(function () {
+//         text1Typing.classList.add('animate-typing');
+//       }, 500); // 0.5초(500밀리초) 지연 후 animate-typing 클래스 추가
+//     }); // 0.5초(500밀리초) 지연 후 show 클래스 추가
+//   }
+// }
+
 window.addEventListener('scroll', handleScroll);
 
 function handleScroll() {
   let text1Typing = document.querySelector('.text1_typing');
   let bounding = text1Typing.getBoundingClientRect();
-  let viewportHeight = window.innerHeight;
 
-  // 스크롤 시 중간쯤 위치할 때 애니메이션 시작 / 1부분일딴 1로해놓고 중간읋 할떄 2로 변경필요
-  if (bounding.top < viewportHeight / 3) {
+  // 스크롤 시작 후 애니메이션 시작
+  if (window.scrollY > 0) {
     setTimeout(function () {
       text1Typing.classList.add('show');
       setTimeout(function () {
         text1Typing.classList.add('animate-typing');
-      }, 500); // 0.5초(500밀리초) 지연 후 animate-typing 클래스 추가
-    }); // 0.5초(500밀리초) 지연 후 show 클래스 추가
+      }, 1000); // 0.5초(500밀리초) 지연 후 animate-typing 클래스 추가
+    }); // 0초 지연 후 show 클래스 추가
   }
 }
 
